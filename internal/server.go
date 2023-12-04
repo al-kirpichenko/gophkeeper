a7613bba6595de7da55c0fc22df3881b9a59e044
al-kirpichenko <fuluganchik@yandex.ru>
2023-12-04 22:06:37 +0700

    добавлен сервер

--- internal/server.go
+++ internal/server.go
@@ -1,4 +1,21 @@
 package internal
 
+import (
+	"gorm.io/gorm"
+
+	"github.com/al-kirpichenko/gophkeeper/cmd/server/config"
+	"github.com/al-kirpichenko/gophkeeper/internal/database"
+)
+
 type Server struct {
+	Config *config.Config
+	DB     *gorm.DB
+}
+
+func NewServer(config *config.Config) *Server {
+
+	return &Server{
+		Config: config,
+		DB:     database.InitDB(config.DataBaseURI),
+	}
 }
